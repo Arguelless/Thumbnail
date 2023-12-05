f997a95eaf8943132ac2faad58d2f788ba54c317
Arguelless <marguelles@protonmail.com>
2023-12-05 18:25:28 +0100

    asdf

---
+++ app/src/main/java/com/arguelles/thumbnail/MainActivity.java
@@ -0,0 +1,63 @@
+package com.arguelles.thumbnail;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityResultLauncher<Intent> someActivityResultLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            if (data != null) {
+                                Uri uri = data.getData();
+                                ImageView imageView = findViewById(R.id.imgView);
+                                imageView.setImageURI(uri);
+                            }
+                        }
+                    }
+                });
+
+        // Obtener referencia al botón
+        Button btnOpenGallery = findViewById(R.id.btnOpenGallery);
+
+        // Establecer OnClickListener para el botón
+        btnOpenGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+    }
+
+    private void openGallery() {
+        // Crear Intent
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/jpg");
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        //Launch activity to get result
+        someActivityResultLauncher.launch(intent);
+    }
+}
+
